4a44df02bbf93ae46987010dcd3d80fcfd24b7d8
Luis Nell <de.da.de@originell.org>
2009-09-20 15:33:51 +0200

    small style adjustment

--- xbmcclient.py
+++ xbmcclient.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+# This file is part of xbmcpd.
+
+# xbmcpd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# xbmcpd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with xbmcpd.  If not, see <http://www.gnu.org/licenses/>.
 """
 Implementation of XBMC's UDP based input system.
 
